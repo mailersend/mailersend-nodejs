471aaaaa2d627c9327ce75208f8b2b4e39109614
devjavi <javieracj4@gmail.com>
2021-05-20 16:17:54 -0600

    Add Token endpoints

--- src/MailerSend.js
+++ src/MailerSend.js
@@ -8,6 +8,13 @@ let headers = {
   "Content-type": "application/json",
 };
 
+const checkTokenId = (token_id) => {
+  if (!token_id) {
+    let err = new Error("Please provide a valid token_id");
+    throw err;
+  }
+};
+
 module.exports = class MailerSend {
   constructor(config) {
     this.api_key = config.api_key;
@@ -80,5 +87,107 @@ module.exports = class MailerSend {
       }
     );
     return response.data;
+  }
+
+  //TOKENS
+  async createToken(token) {
+    let err;
+    const { name, scopes, domain_id } = token;
+    const scopesArray = [
+      "email_full",
+      "domains_read",
+      "domains_full",
+      "activity_read",
+      "activity_full",
+      "analytics_read",
+      "analytics_full",
+      "tokens_full",
+    ];
+
+    //Check for required values name and scopes
+    if (!name || !scopes || !domain_id) {
+      err = new Error(
+        "Please include a name, domain_id and scopes in your request"
+      );
+      throw err;
+    }
+
+    //Check if scopes have values
+    if (scopes.length < 1) {
+      err = new Error("You need to add at least 1 scope");
+      throw err;
+    }
+
+    //Check if all values in scopes array are valid.
+    for (let i = 0; i < scopes.length; i++) {
+      const scope = scopes[i];
+      if (!scopesArray.includes(scope)) {
+        err = new Error(`"${scope}" is not a valid value for scopes`);
+        throw err;
+      }
+    }
+
+    //Check if name value is not bigger than 191 characters
+    if (name.length > 191) {
+      err = new Error("Token name has to be less than 191 characters");
+      throw err;
+    }
+
+    const response = await axios.post(this.basePath + "/token", token, {
+      headers,
+    });
+    return response.data.data;
+  }
+
+  //Pause Token
+  async pauseToken(token_id) {
+    let err;
+
+    //Check if token_id was provided
+    if (!token_id) {
+      err = new Error("Please provide a valid token_id");
+      throw err;
+    }
+
+    const response = await axios.put(
+      `${this.basePath}/token/${token_id}/settings`,
+      {
+        status: "pause",
+      },
+      {
+        headers,
+      }
+    );
+
+    return response.data.data;
+  }
+
+  //Unpause Token
+  async unpauseToken(token_id) {
+    //Check if token_id was provided
+    checkTokenId(token_id);
+
+    const response = await axios.delete(
+      `${this.basePath}/token/${token_id}/settings`,
+      {
+        status: "unpause",
+      },
+      {
+        headers,
+      }
+    );
+
+    return response.data;
+  }
+
+  async deleteToken(token_id) {
+    //Check if token_id was provided
+    checkTokenId(token_id);
+
+    const response = await axios.delete(`${this.basePath}/token/${token_id}`, {
+      headers,
+    });
+
+    return response;
   }
 };
