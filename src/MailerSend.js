919ee19880530a644bb6de1604a33f2bc06130a9
Arunas Smaliukas <arunas@smala.lt>
2021-05-21 13:33:54 +0300

    stick with axios for all requests

--- src/MailerSend.js
+++ src/MailerSend.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const fetch = require("isomorphic-unfetch");
 const axios = require("axios");
 
 let headers = {
@@ -15,38 +14,26 @@ module.exports = class MailerSend {
     headers.Authorization = `Bearer ${this.api_key}`;
   }
 
-  request(endpoint = "", options = {}) {
-    const url = this.basePath + endpoint;
-
-    const config = {
-      headers,
-      ...options,
-    };
-
-    return fetch(url, config);
-  }
-
   //EMAILS
   send(emailParams) {
-    return this.request("/email", {
-      method: "POST",
-      body: JSON.stringify({
-        from: {
-          email: emailParams.from,
-          name: emailParams.fromName,
-        },
-        to: emailParams.recipients,
-        cc: emailParams.cc,
-        bcc: emailParams.bcc,
-        attachments: emailParams.attachments,
-        subject: emailParams.subject,
-        text: emailParams.text,
-        html: emailParams.html,
-        template_id: emailParams.templateId,
-        variables: emailParams.variables,
-        personalization: emailParams.personalization,
-        tags: emailParams.tags,
-      }),
+    return axios.post(this.basePath + "/email", {
+      from: {
+        email: emailParams.from,
+        name: emailParams.fromName,
+      },
+      to: emailParams.recipients,
+      cc: emailParams.cc,
+      bcc: emailParams.bcc,
+      attachments: emailParams.attachments,
+      subject: emailParams.subject,
+      text: emailParams.text,
+      html: emailParams.html,
+      template_id: emailParams.templateId,
+      variables: emailParams.variables,
+      personalization: emailParams.personalization,
+      tags: emailParams.tags,
+    }, {
+      headers
     });
   }
 
