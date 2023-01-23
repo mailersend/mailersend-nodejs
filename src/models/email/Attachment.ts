784a197713b0208bd1655783d5c6af8ed9f45dd4
Jonas Frisell <jonas@frisell.it>
2023-01-23 07:13:45 +0100

    Added missing id parameter for attachments

--- src/models/email/Attachment.ts
+++ src/models/email/Attachment.ts
@@ -2,10 +2,12 @@ export class Attachment {
   content: string;
   filename: string;
   disposition: string;
+  id?: string;
 
-  constructor(content: string, fileName: string, disposition: string = "attachment") {
+  constructor(content: string, fileName: string, disposition: string = "attachment", id?: string) {
     this.content = content;
     this.filename = fileName;
     this.disposition = disposition;
+    this.id = id;
   }
 }
