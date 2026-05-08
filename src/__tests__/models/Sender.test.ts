b8ccb3312b0cd5247fb5a35f448137e243854c9d
Joao Cruz <joao.fs.cruz97@gmail.com>
2026-05-08 11:54:21 +0100

    chore(tests): remove model test files that only test plain assignments

--- src/__tests__/models/Sender.test.ts
+++
@@ -1,9 +0,0 @@
-import { Sender } from "../../models";
-
-describe("Sender Model", () => {
-  it("Constructor", () => {
-    const sender = new Sender("sender@mail.com", "Michel");
-    expect(sender.email).toBe("sender@mail.com");
-    expect(sender.name).toBe("Michel");
-  });
-});
