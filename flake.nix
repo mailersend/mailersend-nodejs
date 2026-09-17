410d24d084cf0e07fdfcf4eb152c98cb01bbf663
Igor Hrček <igor@hrcek.rs>
2026-09-17 14:45:11 +0200

    feat: Added gitleaks hook

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "MailerLite Go SDK environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+      forEachSystem = f:
+        nixpkgs.lib.genAttrs systems (system: f system nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSystem (system: pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            lefthook
+            gitleaks
+            git-secrets
+          ];
+        };
+      });
+    };
+}
